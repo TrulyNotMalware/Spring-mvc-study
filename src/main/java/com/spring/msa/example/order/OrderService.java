391bf2365b4b2598ce459425a065e8c25d2141e3
TrulyNotMalware <notleebutyee@gmail.com>
2022-09-29 16:44:29 +0900

    Study- MSA Patterns ~ gRPC supports

---
+++ src/main/java/com/spring/msa/example/order/OrderService.java
@@ -0,0 +1,10 @@
+package com.spring.msa.example.order;
+
+import io.eventuate.tram.events.aggregates.ResultWithDomainEvents;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class OrderService {
+
+
+}
